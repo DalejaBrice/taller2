62526f9710dc4ea0ef0d1cdaa32a29b492d20c2b
DalejaBri <dalejabriceno@gmail.com>
2024-10-18 20:49:49 -0500

    segundo commit

--- proyecto01/src/main/java/com/mycompany/proyecto01/Proyecto01.java
+++ proyecto01/src/main/java/com/mycompany/proyecto01/Proyecto01.java
@@ -3,6 +3,6 @@ package com.mycompany.proyecto01;
 public class Proyecto01 {
 
     public static void main(String[] args) {
-        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+        System.out.println("Ejemplo de java creado por DalejaBrice");
     }
 }
